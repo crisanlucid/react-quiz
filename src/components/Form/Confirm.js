f4cd1968eb9afb55b6a7be341e36a9607096afce
crisanlucid <crisan.lucid@yahoo.com>
2019-02-22 14:38:48 +0100

    update config JSON to have same props

---
+++ src/components/Form/Confirm.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Button from '../UI/Button/Button';
+import Input from '../UI/Input/Input';
+import './Form.scss';
+
+const formConfirm = props => {
+  const { values, handleChange } = props;
+
+  const fieldsForm = values;
+  const continueHandler = e => {
+    e.preventDefault();
+    props.nextStep();
+  };
+
+  const backHandler = e => {
+    e.preventDefault();
+    props.prevStep();
+  };
+
+  return (
+    <React.Fragment>
+      <div className="Form__wrapper--quiz">
+        {Object.values(fieldsForm).map((config, index) => (
+          <div key={index} className="form-quiz">
+            To do:
+          </div>
+        ))}
+      </div>
+      <div className="form-group">
+        <Button
+          disabled={false}
+          label="Back"
+          btnType=""
+          onClick={backHandler}
+        />
+        <Button
+          label="Finish"
+          btnType="Success"
+          disabled={false}
+          onClick={continueHandler}
+        />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default formConfirm;
